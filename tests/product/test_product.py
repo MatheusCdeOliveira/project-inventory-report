de8083735330fa0f7f507d497093318bca518cd7
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-08 14:45:25 -0300

    iniciando projeto && req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(1, "i5 13600K", "Intel", "13/02/2023", "NA", 45, "No")
+    assert product.id == 1
+    assert product.nome_do_produto == "i5 13600K"
+    assert product.nome_da_empresa == "Intel"
+    assert product.data_de_fabricacao == "13/02/2023"
+    assert product.data_de_validade == "NA"
+    assert product.numero_de_serie == 45
+    assert product.instrucoes_de_armazenamento == "No"
