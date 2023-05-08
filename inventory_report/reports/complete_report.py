c02e29c283a15bdc1b564f745b0609da298e7203
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-08 18:31:02 -0300

    req 03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        counts_by_name = Counter(
+            d["nome_da_empresa"] for d in data
+        ).most_common()
+        stock = ''
+        for x in counts_by_name:
+            stock += (f"- {x[0]}: {x[1]}\n")
+        return (
+            f"{SimpleReport.generate(data)}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{stock}"
+        )
