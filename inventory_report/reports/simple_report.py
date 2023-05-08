b995164b635f680bdbc8f55913408ac28cacc9b9
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-08 17:07:22 -0300

    req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data):
+        today = datetime.today().date()
+        counts_by_name = Counter(
+            d["nome_da_empresa"] for d in data
+        ).most_common(1)[0][0]
+        lst_oldest_date = []
+        lst_closest_date = []
+        for obj in data:
+            d = datetime.strptime(obj["data_de_fabricacao"], "%Y-%m-%d").date()
+            v = datetime.strptime(obj["data_de_validade"], "%Y-%m-%d").date()
+            lst_oldest_date.append(d)
+            if v > today:
+                lst_closest_date.append(obj["data_de_validade"])
+        return f"""Data de fabricação mais antiga: {min(lst_oldest_date)}
+Data de validade mais próxima: {min(lst_closest_date)}
+Empresa com mais produtos: {counts_by_name}"""
